9073e99fe220b4fc0a25ebd5e8b17ac17c478294
Dan Werner <danwerner21@gmail.com>
2022-10-15 22:40:53 -0500

    unicorn work

--- unicornboard/UnicornFirmware/UnicornFirmware/src/main.cpp
+++ unicornboard/UnicornFirmware/UnicornFirmware/src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include "grom.h"
 
 #define   D0 26  //25
 #define   D1 13
@@ -29,6 +30,7 @@ volatile       uint8_t d7;
 
 volatile       uint8_t mo;
 volatile       uint8_t m;
+volatile       uint8_t outbyte;
 
 
 void setup() {
@@ -47,7 +49,7 @@ void setup() {
   pinMode(GREADY,OUTPUT_OPEN_DRAIN );
   pinMode(M, INPUT);
   digitalWrite(GREADY,0);
-  printf("STARTUP \n");
+//  printf("STARTUP \n");
 }
 
 void loop() {
@@ -60,6 +62,7 @@ void loop() {
     // SET ADDRESS
     if((mo==1) && (m==0))
     {
+
       d0=digitalRead(D0);
       d1=digitalRead(D1);
       d2=digitalRead(D2);
@@ -70,19 +73,16 @@ void loop() {
       d7=digitalRead(D7);
 
       dataByte = d0 | (d1 << 1) | (d2 << 2) | (d3 << 3) | (d4 << 4) | (d5 << 5) | (d6 << 6) | (d7 << 7);
-///      printf("Address Byte %i %i %i %i %i %i %i %i\n",d7,d6,d5,d4,d3,d2,d1,d0);
       if(lowByte)
       {
         address=(dataByte <<8);
         lowByte=false;
-        printf("Address HIGH %02x \n",dataByte);
       }
       else
       {
         address=address | dataByte;
-        printf("Address LOW %02x \n",dataByte);
         lowByte=true;
-        printf("Address %04x \n",address);
+     //   printf("address->%04x\n",address);
       }
     }
 
@@ -95,8 +95,8 @@ void loop() {
     // Read Data
     if((mo==0) && (m==1))
     {
+/* This is the code to monitor other GROMS
       delayMicroseconds(4);
-
       d0=digitalRead(D0);
       d1=digitalRead(D1);
       d2=digitalRead(D2);
@@ -105,14 +105,52 @@ void loop() {
       d5=digitalRead(D5);
       d6=digitalRead(D6);
       d7=digitalRead(D7);
-
       dataByte = d0 | (d1 << 1) | (d2 << 2) | (d3 << 3) | (d4 << 4) | (d5 << 5) | (d6 << 6) | (d7 << 7);
-      printf("Data Byte %02x %i %i %i %i %i %i %i %i\n",dataByte,d7,d6,d5,d4,d3,d2,d1,d0);
+      */
+      pinMode(D0, OUTPUT);
+      pinMode(D1, OUTPUT);
+      pinMode(D2, OUTPUT);
+      pinMode(D3, OUTPUT);
+      pinMode(D4, OUTPUT);
+      pinMode(D5, OUTPUT);
+      pinMode(D6, OUTPUT);
+      pinMode(D7, OUTPUT);
+      outbyte=GROM[address];
+      digitalWrite(D0,outbyte & 0x01);
+      digitalWrite(D1,(outbyte>>1) & 0x01);
+      digitalWrite(D2,(outbyte>>2) & 0x01);
+      digitalWrite(D3,(outbyte>>3) & 0x01);
+      digitalWrite(D4,(outbyte>>4) & 0x01);
+      digitalWrite(D5,(outbyte>>5) & 0x01);
+      digitalWrite(D6,(outbyte>>6) & 0x01);
+      digitalWrite(D7,(outbyte>>7) & 0x01);
+
+    //  printf("d-> %i %i %i %i %i %i %i %i -- ",(outbyte & 0x01),
+   //   ((outbyte>>1) & 0x01),
+   //   ((outbyte>>2) & 0x01),
+   //   ((outbyte>>3) & 0x01),
+   //   ((outbyte>>4) & 0x01),
+   //   ((outbyte>>5) & 0x01),
+   //   ((outbyte>>6) & 0x01),
+   //   ((outbyte>>7) & 0x01));
+
+//      printf("add->%04x, data->%02x\n",address,outbyte);
+      address++;
+      //need to add roll over
     }
 
     digitalWrite(GREADY,1);
     while(digitalRead(GS)==0);
     digitalWrite(GREADY,0);
+    delayMicroseconds(1);
+      pinMode(D0, INPUT);
+      pinMode(D1, INPUT);
+      pinMode(D2, INPUT);
+      pinMode(D3, INPUT);
+      pinMode(D4, INPUT);
+      pinMode(D5, INPUT);
+      pinMode(D6, INPUT);
+      pinMode(D7, INPUT);
   }
   else
   {
